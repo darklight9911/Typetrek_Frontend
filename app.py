3f0fe5ae34e41aef98ebba5104c06e87bca6349c
darklight9911 <muniaedu911@gmail.com>
2024-12-18 23:37:42 +0600

    added login and registration page

--- app.py
+++ app.py
@@ -3,5 +3,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+@app.route("/registration")
+def registrationPage():
+    return render_template("registration.html")
+@app.route("/login")
+def loginPage():
+    return render_template("login.html")
+
 if __name__=="__main__":
     app.run(debug=True,port=4545,host='localhost')
