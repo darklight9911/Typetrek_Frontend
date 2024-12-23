0c91eb74cb82f869a4fbf4e27de5920fa0133795
darklight9911 <muniaedu911@gmail.com>
2024-12-24 01:02:24 +0600

    added login mechanism

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask,render_template,flash,session,url_for
+
+from flask import Flask,render_template,flash,session,url_for, request, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "adkasdkljaskldjklajdklajsdkljaklsdjasd"
 @app.route("/")
 def index():
-    flash(" this an error message this an error message this an error message this an error message","info")
     return render_template("index.html")
 @app.route("/registration")
 def registrationPage():
     return render_template("registration.html")
-@app.route("/login")
+@app.route("/login",methods = ["GET","POST"])
 def loginPage():
+    if request.method == "POST":
+        print(request.form)
+        if request.form.get('email')=="admin@gmail.com":
+            if request.form.get('password')=="1234":
+                session['user_logged_in'] =True
+                flash("Login successful",'success')
+                return redirect("/")
+            flash("Wrong password","error")
+            return redirect("/login")
+        flash("Wrong email","error") 
+        return redirect("/login")
     return render_template("login.html")
-
+@app.route('/logout')
+def logout():
+    session['user_logged_in'] =False
+    flash("Logout successful","success")
+    return redirect("/login")
 if __name__=="__main__":
     app.run(debug=True,port=4545,host='localhost')
